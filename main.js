2d1e6cb64618e9b7b25b6d0c63b89402cd2f302a
Kenneth Lewis <kennethlewis@Kenneths-MacBook-Pro.local>
2017-09-05 20:22:44 -0500

    added more stuff

--- main.js
+++ main.js
@@ -1,39 +1,10 @@
-// console.log("in main.js");
-
-// let Something = (function(oldSomething) {
-// 	const animals = ["cat","dog"];
-// 	oldSomthing.niceFunction = function() {
-// 		console.log("I'm a nice function");
-// 	}
-// 	return oldSomthing;
-
-// })(Something || {});
-
-
-var SolarSystem = (function(oldSolarSystem){
-	const planets = ["mercury","venus","earth","mars","jupiter","saturn","uranus","neptune"];
-	const spaceCrafts = ["mercury","voyager","challenger","apollo","spunknik","mars rover","enterpirse","millennium falcon"];
-
-	oldSolarSystem.getPlanets = function() {
-		return planets;
-	//	console.log("planets", planets);
-	};
-
-	oldSolarSystem.setPlanet = function(newPlanet) {
-		planets.push(newPlanet);
-	}
-
-	oldSolarSystem.getSpaceCrafts = function() {
-		return spaceCrafts;
-	//	console.log("space crafts", spaceCrafts);
-	};
-
-	oldSolarSystem.setSpaceCraft = function(newSpaceCraft) {
-		spaceCrafts.push(newSpaceCraft);
-	}
-
-	return oldSolarSystem;
-})(SolarSystem || {});
+SolarSystem.setPlanet("Planet X");
 
 let myPlanets = SolarSystem.getPlanets();
 console.log("myPlanets" , myPlanets);
+
+let mySpacecraft = SolarSystem.getSpacecrafts();
+console.log("mySpacecraft", mySpacecraft);
+
+let myStars = SolarSystem.getStars();
+console.log("myStars", myStars);
---
+++ planets.js
@@ -0,0 +1,14 @@
+var SolarSystem = (function(oldSolarSystem){
+	const planets = ["mercury","venus","earth","mars","jupiter","saturn","uranus","neptune"];
+
+	oldSolarSystem.getPlanets = function() {
+		return planets;
+	//	console.log("planets", planets);
+	};
+
+	oldSolarSystem.setPlanet = function(newPlanet) {
+		planets.push(newPlanet);
+	}
+
+	return oldSolarSystem;
+})(SolarSystem || {});
